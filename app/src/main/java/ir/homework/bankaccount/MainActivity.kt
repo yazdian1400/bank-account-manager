2391011113188efd70c5ff6fd2989889b15f97e7
Maedeh <yazdian.oul@gmail.com>
2022-04-13 17:41:43 +0430

    add primary drawer menu

--- app/src/main/java/ir/homework/bankaccount/MainActivity.kt
+++ app/src/main/java/ir/homework/bankaccount/MainActivity.kt
@@ -2,10 +2,59 @@ package ir.homework.bankaccount
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+
+import com.google.android.material.navigation.NavigationView
+import ir.homework.bankaccount.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var binding: ActivityMainBinding
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        drawerLayout= binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_profile, R.id.nav_create_accounts, R.id.nav_show_accounts, R.id.nav_select_account
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        navView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { item ->
+            if (item.itemId == R.id.nav_delete_all) {
+                finish()
+            }
+            else {
+                NavigationUI.onNavDestinationSelected(item, navController)
+                drawerLayout.closeDrawers()
+            }
+            false
+        })
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 }
